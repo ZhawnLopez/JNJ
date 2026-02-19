1f978b224f70f64263f0b46c27e8be0224be9345
ZhawnLopez <zblopez@student.apc.edu.ph>
2026-02-19 20:09:55 +0800

    created backend folder and files
    no actual content inside files yet ill be finishing it and committing when done

---
+++ backend/chefint.php
@@ -0,0 +1,5 @@
+sees list of orders and list of already accepted orders (by other chefs)
+has page that displays ingredients, can tag ingredients as 'need restock'
+can set order status as 'prepared'
+
+wait ill finalize this in vscode
